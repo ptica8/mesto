82c24cfb77678d111b6cd5830f817b2a4ec98001
Valeria Mikshina <lera-mikshina@mail.ru>
2022-03-12 20:47:44 +0300

    after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,33 @@
 const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.profile__edit');
 const closePopup = popup.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__text_type_name');
+let jobInput = formElement.querySelector('.popup__text_type_job');
+let nameOutput = document.querySelector('.profile__name');
+let jobOutput = document.querySelector('.profile__subtitle');
 
-function togglePopup(event) {
-    popup.classList.toggle('popup__opened');
+function startPopup() {
+    popup.classList.add('popup_opened');
 }
-
-popup.addEventListener('click', function (event) {
-    if (event.target === event.currentTarget) {
-        togglePopup();
-    }
-});
-
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
-
-let formElement = document.querySelector('.input');
-let nameInput = formElement.querySelector('.input__name');
-let jobInput = formElement.querySelector('.input__job');
+function endPopup() {
+    popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
-    let nameOutput = document.querySelector('.profile__name');
-    let jobOutput = document.querySelector('.profile__subtitle');
-    nameOutput.textContent = nameInput.value ;
+    nameOutput.textContent = nameInput.value;
     jobOutput.textContent = jobInput.value;
+    endPopup();
 }
-popup.addEventListener('submit', formSubmitHandler);
+
+openPopup.addEventListener('click', startPopup);
+closePopup.addEventListener('click', endPopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
+/* function eventClose(event) {
+    if (event.target === event.currentTarget) {
+        endPopup();
+    }
+}
+popup.addEventListener('click', eventClose); */
