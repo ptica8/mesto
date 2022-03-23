9afdd2b49cd829feb7c9890c6b7581d1540ff32a
Valeria Mikshina <lera-mikshina@mail.ru>
2022-03-23 17:11:49 +0300

    feat: add list

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,44 @@
-const popup = document.querySelector('.popup');
-const openPopup = document.querySelector('.profile__edit');
-const closePopup = popup.querySelector('.popup__close');
+ const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const list = document.querySelector('.list');
+const listTemplate = document.querySelector('.list-template').content.querySelector('.list__element');
+
+initialCards.forEach(item => {
+    const listElement = listTemplate.cloneNode(true);
+    const listElementImg = listElement.querySelector('.list__element-img');
+    listElementImg.src = item.link;
+    const listElementTitle = listElement.querySelector('.card__element-title');
+    listElementTitle.textContent = item.name;
+    list.append(listElement);
+});
+
+
+
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__text_type_name');
 let jobInput = formElement.querySelector('.popup__text_type_job');
@@ -28,9 +66,3 @@ openPopup.addEventListener('click', startPopup);
 closePopup.addEventListener('click', endPopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
-/* function eventClose(event) {
-    if (event.target === event.currentTarget) {
-        endPopup();
-    }
-}
-popup.addEventListener('click', eventClose); */
